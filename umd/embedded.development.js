e323640e5900ff546dc0f4f761ebf4185e7705c9
James Adler <james@nyaasllc.com>
2019-08-21 19:17:26 -0500

    temp fix

--- umd/embedded.development.js
+++ umd/embedded.development.js
@@ -6643,7 +6643,7 @@ function updateLink (link, options, obj) {
 
 	if (sourceMap) {
 		// http://stackoverflow.com/a/26603875
-		css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+		css += "\n/*# surceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
 	}
 
 	var blob = new Blob([css], { type: "text/css" });
